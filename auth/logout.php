82acf9723eb6851610e60638bde1a2d623bf7478
MDMilano <marcdanielmilano@gmail.com>
2025-05-14 20:54:28 +0800

    header location updated

--- auth/logout.php
+++ auth/logout.php
@@ -3,7 +3,7 @@ session_start();
 session_unset();
 session_destroy();
 
-// header("Location: ../index.php");
+// header("Location: ../");
 echo "<script>alert('Successfully logout!'); window.location.href='../';</script>";
 exit;
 ?>
